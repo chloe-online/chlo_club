aa53480d4da4b1dcbd10b480a2d18f2e79e48ad1
andii-online <andistraus99@gmail.com>
2025-02-24 13:42:12 -0500

    update favi and page to be wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<h1>chlo.club</h1>
+<div class="message-container">
+	<p>check in soon</p>
+	<p>love ya,</p>
+	<p><i>chloe</i></p>
+</div>
+
+<style>
+	.message-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		font-size: 2rem;
+		color: #000;
+		text-align: center;
+	}
+</style>
